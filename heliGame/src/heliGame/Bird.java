ac66ad132569c2608b76a733754d57aefe09dbdd
Andrew Paettie <ajp073000@utdallas.edu>
2013-04-01 13:07:56 -0500

    Fill level 1 with trees and rings; make number of birds less when DEBUG is true

--- heliGame/src/heliGame/Bird.java
+++ heliGame/src/heliGame/Bird.java
@@ -11,13 +11,15 @@ import java.util.Random;
 public class Bird extends Sprite {
 	//---------constants-----------
 	private static final String IMAGE_NAME = "birdSprite.gif";
-	static final int MAX_BIRDS = 15;
+	
 	static final int MIN_ALTITUDE = 450;//the lowest they will go
 	static final int BIRD_WIDTH = 20;
 	static final int BIRD_HEIGHT = 20;
 	static final int MAX_SPEED = 3;
+        static final int MAX_BIRDS_NORMAL = 35;
+        static final int MAX_BIRDS_DEBUG = 15;
 	
-	
+	static int MAX_BIRDS = MAX_BIRDS_NORMAL;
 	//---------variables------------
 	private int canvasX;
 	private int initialY;
@@ -25,12 +27,21 @@ public class Bird extends Sprite {
 	//----------------------------stuff for animation-------------------------------
 	private static final int [] birdAnimImageNumbers = {1,2,3};
 	private static final String birdAnimLocation = "images/bird/birdFrame";
-	private static final int birdAnimDelay = MAX_BIRDS * 10;
+	private static int birdAnimDelay = MAX_BIRDS * 10;
 	private static AnimationUtils birdAnim;
 	
 	//--------------constructor--------------
 	Bird(int xIn,int yIn, boolean initAnimation){
-		super(xIn,yIn);
+                super(xIn,yIn);
+                if(HeliGameMain.DEBUG){
+                        Bird.MAX_BIRDS = MAX_BIRDS_DEBUG;
+                        birdAnimDelay = MAX_BIRDS * 10;
+                }
+                else{
+                        Bird.MAX_BIRDS = MAX_BIRDS_NORMAL;
+                        birdAnimDelay = MAX_BIRDS * 10;
+                }
+                
 		if (initAnimation){
 			birdAnim = new AnimationUtils(birdAnimImageNumbers.length,
                                                   birdAnimLocation,
